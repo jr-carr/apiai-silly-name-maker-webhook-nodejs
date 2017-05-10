f39fca4f91ac4ec402c45c676674306199c364df
jr-carr <jcarr@carrsolutions.com>
2017-05-10 12:29:17 -0400

    Update index.js

--- index.js
+++ index.js
@@ -26,6 +26,21 @@ exports.sillyNameMaker = (req, res) => {
   console.log('Request headers: ' + JSON.stringify(req.headers));
   console.log('Request body: ' + JSON.stringify(req.body));
 
+  function saySSML(assistant) {
+  let text_to_speech = '<speak>'
+    + 'Here are <say-as interpret-as="characters">SSML</say-as> samples. '
+    + 'I can pause <break time="3"/>. '
+    + 'I can play a sound <audio src="https://www.example.com/MY_WAVE_FILE.wav">your wave file</audio>. '
+    + 'I can speak in cardinals. Your position is <say-as interpret-as="cardinal">10</say-as> in line. '
+    + 'Or I can speak in ordinals. You are <say-as interpret-as="ordinal">10</say-as> in line. '
+    + 'Or I can even speak in digits. Your position in line is <say-as interpret-as="digits">10</say-as>. '
+    + 'I can also substitute phrases, like the <sub alias="World Wide Web Consortium">W3C</sub>. '
+    + 'Finally, I can speak a paragraph with two sentences. '
+    + '<p><s>This is sentence one.</s><s>This is sentence two.</s></p>'
+    + '</speak>'
+  assistant.tell(text_to_speech);
+};
+  
   // Make a silly name
   function makeName (assistant) {
     let number = assistant.getArgument(NUMBER_ARGUMENT);
